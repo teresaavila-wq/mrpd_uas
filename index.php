7a6943279cb2f1491ff5a6243e10a0674b0791ad
teresaavila-wq <teresaavila@itbss.ac.id>
2026-06-08 22:30:09 +0700

    update1

--- index.php
+++ index.php
@@ -1,51 +1,8 @@
-<!DOCTYPE html>
-<html lang="id">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale-1.0">
-        <title>Maria Ratu Pencinta Damai Pontianak</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        
-        <!--NAVIGATION BAR tere-->
-         <div class="menu-toggle"></div>
+<?php
+include "header.php";
+?>
 
-        <header class="navbar">
-            <div class="nav-left">
-                <img src="images/logomrpd.PNG" alt="logomrpd" class="logomrpd-img">
-            <span class="logo-text">Maria Ratu Pencinta Damai Pontianak</span>
-            </div>
-
-            <nav class="menu">
-                <a href="#">Beranda</a>
-                <a href="#jadwal">Jadwal</a>
-                <a href="#sejarah">Sejarah</a>
-                <a href="#pengurus">Pengurus</a>
-                <a href="#galeri">Galeri</a>
-                <a href="#lokasi">Lokasi</a>
-                <a href="#kontak">Kontak</a>
-                <a href="#informasi">Informasi</a>
-            </nav>
-        </header>
-
-        <!--HERO-->
-        <section class="hero">
-            <div class="overlay"></div>
-
-            <div class="hero-content">
-                <h1>Maria Ratu Pencinta Damai Pontianak</h1>
-                <p class="sub">
-                    Komunitas iman, harapan, dan kasih
-                </p>
-
-                <p class="jadwal-hero">
-                    ⌛Misa Minggu : 08.00 & 18.00
-                </p>
-            </div>
-        </section>
-
-        <section id="jadwal">
+      <section id="jadwal">
             <h2>Jadwal Misa</h2>
 
             <div class="jadwal-container">
