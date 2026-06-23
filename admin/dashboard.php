47602fd4d1eb194278364053c90be803b9323849
teresaavila-wq <teresaavila@itbss.ac.id>
2026-06-23 08:10:10 +0700

    dashboard jenis kegiatan

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,4 +6,6 @@ echo "Selamat datang, ".$username;
 <br>
 <a href="jadwal_misa/index.php">manajemen jadwal misa</a>
 <br>
+<a href="jenis_kegiatan/index.php">manajemen jenis kegiatan</a>
+<br>
 <a href="logout.php">logout</a>
